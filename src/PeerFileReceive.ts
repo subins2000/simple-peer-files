f6aabe3fab6bf7766855f0504625c601f642bc5d
Subin Siby <mail@subinsb.com>
2020-08-13 02:57:38 +0530

    Add method to get receiving file data as stream

--- src/PeerFileReceive.ts
+++ src/PeerFileReceive.ts
@@ -1,4 +1,4 @@
-import { Writable } from 'readable-stream'
+import { Readable, Writable } from 'readable-stream'
 import { EventEmitter } from 'ee-ts'
 import SimplePeer from 'simple-peer'
 
@@ -40,12 +40,14 @@ class ReceiveStream extends Writable {
       this.emit('chunk', data.slice(1))
     } else if (data[0] === ControlHeaders.TRANSFER_PAUSE) {
       this.emit('paused')
-    } else if (data[0] === ControlHeaders.TRANSFER_CANCEL) {
+    }
+    
+    if (data[0] === ControlHeaders.TRANSFER_CANCEL) {
       this.emit('cancelled')
       this.destroy()
+    } else {
+      cb(null) // Signal that we're ready for more data
     }
-
-    cb(null) // Signal that we're ready for more data
   }
 }
 
@@ -59,13 +61,70 @@ export default class PeerFileReceive extends EventEmitter<Events> {
 
   private fileName: string;
   private fileSize!: number; // File size in bytes
+  private fileData = [];
+  private fileStream: Readable = null;
   private fileType!: string;
-  private receivedData: Uint8Array[] = [];
 
   constructor (peer: SimplePeer.Instance) {
     super()
 
     this.setPeer(peer)
+  }
+
+  // When peer is changed, start a new stream handler and assign events
+  setPeer (peer: SimplePeer.Instance) {
+    if (this.rs) {
+      this.rs.destroy()
+    }
+
+    this.rs = new ReceiveStream()
+    this.peer = peer
+
+    peer.pipe(this.rs)
+
+    this.rs.on('start', meta => {
+      this.fileName = meta.fileName
+      this.fileSize = meta.fileSize
+      this.fileType = meta.fileType
+      this.fileData = []
+    })
+    this.rs.on('chunk', chunk => {
+      this.fileData.push(chunk)
+
+      if (this.fileStream) {
+        this.fileStream.push(chunk)
+      }
+
+      this.bytesReceived += chunk.byteLength
+
+      if (this.bytesReceived === this.fileSize) {
+        // completed
+        this.sendPeer(ControlHeaders.FILE_END)
+
+        if (this.fileStream) this.fileStream.push(null) // EOF
+
+        const file = new window.File(
+          this.fileData,
+          this.fileName,
+          {
+            type: this.fileType
+          }
+        )
+
+        this.emit('progress', 100.0, this.fileSize)
+        this.emit('done', file)
+      } else {
+        const percentage = parseFloat((100 * (this.bytesReceived / this.fileSize)).toFixed(3))
+
+        this.emit('progress', percentage, this.bytesReceived)
+      }
+    })
+    this.rs.on('paused', () => {
+      this.emit('paused')
+    })
+    this.rs.on('cancelled', () => {
+      this.emit('cancelled')
+    })
   }
 
   /**
@@ -88,6 +147,15 @@ export default class PeerFileReceive extends EventEmitter<Events> {
     this.peer.send(resp)
   }
 
+  // Create a stream for receiving file data
+  createReadStream () {
+    this.fileStream = new Readable({
+      objectMode: true,
+      read () {} // We'll be using push when we have file chunk
+    })
+    return this.fileStream
+  }
+
   // Request sender to pause transfer
   pause () {
     this.sendPeer(ControlHeaders.TRANSFER_PAUSE)
@@ -103,59 +171,12 @@ export default class PeerFileReceive extends EventEmitter<Events> {
   }
 
   cancel () {
+    this.cancelled = true
     this.sendPeer(ControlHeaders.TRANSFER_CANCEL)
 
     this.rs.destroy()
     this.peer.destroy()
 
     this.emit('cancel')
-  }
-
-  // When peer is changed, start a new stream handler and assign events
-  setPeer (peer: SimplePeer.Instance) {
-    if (this.rs) {
-      this.rs.destroy()
-    }
-
-    this.rs = new ReceiveStream()
-    this.peer = peer
-
-    peer.pipe(this.rs)
-
-    this.rs.on('start', meta => {
-      this.fileName = meta.fileName
-      this.fileSize = meta.fileSize
-      this.fileType = meta.fileType
-    })
-    this.rs.on('chunk', chunk => {
-      this.receivedData.push(chunk)
-      this.bytesReceived += chunk.byteLength
-
-      if (this.bytesReceived === this.fileSize) {
-        // completed
-        this.sendPeer(ControlHeaders.FILE_END)
-
-        const file = new window.File(
-          this.receivedData,
-          this.fileName,
-          {
-            type: this.fileType
-          }
-        )
-
-        this.emit('progress', 100.0, this.fileSize)
-        this.emit('done', file)
-      } else {
-        const percentage = parseFloat((100 * (this.bytesReceived / this.fileSize)).toFixed(3))
-
-        this.emit('progress', percentage, this.bytesReceived)
-      }
-    })
-    this.rs.on('paused', () => {
-      this.emit('paused')
-    })
-    this.rs.on('cancelled', () => {
-      this.emit('cancelled')
-    })
   }
 }
